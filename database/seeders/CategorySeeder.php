86f6c84e06ff7b074b71014fc8ceb101cee3f801
Senura Perera <senuradp@gmail.com>
2022-07-26 14:19:13 +0800

    Update CategorySeeder.php

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class CategorySeeder extends Seeder
 {
@@ -14,6 +16,38 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+      $categories = [
+        "Islands",
+        "Beaches",
+        "Amazing pools",
+        "OMG!",
+        "Arctic",
+        "Tiny homes",
+        "Design",
+        "National parks",
+        "Cabins",
+        "Lakefront",
+        "Surfing",
+        "Amazing views",
+        "Caves",
+        "Camping",
+        "Shared homes",
+        "Earth homes",
+        "Tropical",
+        "Bed & breakfasts",
+        "Luxe",
+        "Golfing"
+      ];
+
+      foreach($categories as $key => $category){
+        (new Category())->create([
+            'title'=>$category,
+            'url'=> Str::slug($category),
+            'summary'=>$category,
+            'content'=>$category,
+            'sort_order'=>$key,
+            'status'=>1
+        ]);
+      }
     }
 }
