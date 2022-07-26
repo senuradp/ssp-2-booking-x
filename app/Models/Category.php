bb92f58ffa24a3c9a5d0e9399f5fd9b5b3b820df
Senura Perera <senuradp@gmail.com>
2022-07-26 13:51:20 +0800

    Category model created and  updated with basic fields

---
+++ app/Models/Category.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Category extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected  $attributes = [
+        'sort_order' => 0,
+        'status' => 1
+    ];
+
+    protected  $fillable = [
+        'title',
+        'url',
+        'summary',
+        'content',
+        'sort-order',
+        'status'
+    ];
+
+    protected  $hidden = [];
+
+    protected  $casts = [];
+}
---
+++ database/migrations/2022_07_26_054846_create_categories_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title');
+
+            $table->string('url')->unique();
+
+            $table->longText('summary')->nullable();
+
+            $table->longText('content')->nullable();
+
+            $table->tinyInteger('sort_order')->default(0);
+
+            $table->boolean('status')->default(true);
+
+            $table->timestamps();
+
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};
