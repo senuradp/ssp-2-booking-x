ad38f8cbaa6b3ed9d0c65ace259834aee446e33b
Senura Perera <senuradp@gmail.com>
2022-07-26 13:30:41 +0800

    Models updated with basic fields

--- app/Models/Finance/Booking.php
+++ app/Models/Finance/Booking.php
@@ -4,8 +4,23 @@ namespace App\Models\Finance;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Booking extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected  $attributes = [
+        'sort-order' => 0,
+        'status'  => 0
+    ];
+
+    protected  $fillable = [
+        'sort-order',
+        'status'
+    ];
+
+    protected  $hidden = [];
+
+    protected  $casts = [];
 }
